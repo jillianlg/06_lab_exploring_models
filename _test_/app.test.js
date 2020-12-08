d17131ae39ca8bbbc80933c20804b0e64aaca021
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-08 14:31:49 -0800

    adds POST test

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -1,8 +1,32 @@
 const { request } = require('express');
+// const { Pool } = require('pg');
+// const request = require('supertest');
+// const pool = require('../lib/utils/pool');
 const app = require('./lib/app');
 
+
 describe('app tests', () => {
-  it('test criteria', () => {
-    return request(app);
+  // beforeEach(() => {
+  //   return pool.query(fs.readFileSync('./data/setup.sql', 'utf-8'))
+  // });
+
+  // afterAll(() => {
+  //   return pool.end();
+  // });
+
+  it('POSTs a tea to the table', async() => {
+    const response = await request(app)
+      .post('/api/v1/tea')
+      .send({
+        type: 'black',
+        name: 'Red Dawn',
+        origin: 'China'
+      });
+    expect(response.body).toEqual({
+      id: '1',
+      type: 'black',
+      name: 'Red Dawn',
+      origin: 'China'
+    });
   });
 });
