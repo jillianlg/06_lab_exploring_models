a41d12db5c778e915be0aa5e888fd0766ac3e435
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-08 16:25:20 -0800

    test passed for POST route

--- _test_/app.test.js
+++ _test_/app.test.js
@@ -1,18 +1,18 @@
-const { request } = require('express');
-// const { Pool } = require('pg');
-// const request = require('supertest');
-// const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const fs  = require('fs');
+const pool = require('../lib/utils/pool');
 const app = require('./lib/app');
 
 
 describe('app tests', () => {
-  // beforeEach(() => {
-  //   return pool.query(fs.readFileSync('./data/setup.sql', 'utf-8'))
-  // });
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./data/setup.sql', 'utf-8'));
+  });
 
-  // afterAll(() => {
-  //   return pool.end();
-  // });
+  afterAll(() => {
+    
+    return pool.end();
+  });
 
   it('POSTs a tea to the table', async() => {
     const response = await request(app)
@@ -22,6 +22,7 @@ describe('app tests', () => {
         name: 'Red Dawn',
         origin: 'China'
       });
+      
     expect(response.body).toEqual({
       id: '1',
       type: 'black',
